d91f3de1e6f9f830690c3eddaaf2d6580d28c938
philiptang42 <philiptang42@gmail.com>
2016-03-18 23:16:20 -0400

    added scope.apps to controller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -23,4 +23,18 @@ app.controller('MainController', ['$scope', function($scope) {
     developer: 'Forecast',
     price: 1.99
   };
+  $scope.apps = [
+  {
+    icon: 'img/move.jpg',
+    title: 'MOVE',
+    developer: 'MOVE, Inc.',
+    price: 0.99
+  },
+  {
+    icon: 'img/shutterbugg.jpg',
+    title: 'Shutterbugg',
+    developer: 'Chico Dusty',
+    price: 2.99
+  }
+]
 }]);
